4d59840f432ceaba19d19d651483e7dd790aadfc
Mark <mm@invisible.ru>
2019-05-27 20:13:49 +0300

    test

---
+++ gg_scripts_.js
@@ -0,0 +1,30 @@
+//var clientInfoUrl = "http://82.202.221.106/client_info";
+//var appLink = "https://itunes.apple.com/us/app/old-mans-journey/id1204902987?mt=8";
+
+
+// var appLink = "https://itunes.apple.com/lookup?id=1155618808";
+
+
+// var json = JSON.stringify(appLink);
+
+// var request = new XMLHttpRequest();
+// request.open('GET', appLink);
+// request.responseType = 'json';
+
+// console.log(request);
+
+
+// fetch(appLink)
+//   .then(response => {
+//     return response.json()
+//   })
+//   .then(data => {
+//     // Work with JSON data here
+//     console.log(data)
+//   })
+//   .catch(err => {
+//     // Do something for an error here
+//   })
+
+var msg = 'Hello World';
+console.log(msg);
